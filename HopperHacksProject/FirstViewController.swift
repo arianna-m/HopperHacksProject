930166b75571881084fa2995a3d3e4cc21ce1099
Arianna Martinelli <mary.arianna.martinelli@gmail.com>
2020-07-22 01:25:06 -0400

    Initial Commit

---
+++ HopperHacksProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HopperHacksProject
+//
+//  Created by Arianna Martinelli on 7/22/20.
+//  Copyright © 2020 HopperHacks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HopperHacksProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HopperHacksProject
+//
+//  Created by Arianna Martinelli on 7/22/20.
+//  Copyright © 2020 HopperHacks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
