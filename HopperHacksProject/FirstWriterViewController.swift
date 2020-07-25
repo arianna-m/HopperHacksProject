b8a4e11c1f5406eb94a89863b50bfc10427bcb23
eugeniafeng <eugeniafeng@gmail.com>
2020-07-25 18:22:07 -0400

    Fixed tableview cell

--- HopperHacksProject/FirstWriterViewController.swift
+++ HopperHacksProject/FirstWriterViewController.swift
@@ -8,32 +8,48 @@
 
 import UIKit
 
-class FirstWriterViewController: UIViewController {
+class FirstWriterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+    var names = ["Jane Taylor", "Kyle Tran", "Mary Lane", "Joe Brown", "Jamie Rose", "Chance Kim"]
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         // Do any additional setup after loading the view.
+        self.tableView.reloadData()
     }
-
-}
-
-    var names = ["Jane Taylor", "Kyle Tran", "Mary Lane", "Joe Brown", "Jamie Rose", "Chance Kim"]
-
-extension UIViewController: UITableViewDelegate, UITableViewDataSource {
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return names.count
     }
-
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CellTableViewCell
-        cell?.label.text = names[indexPath.row]
-        cell?.img.image = UIImage(named: names[indexPath.row])
-        return cell!
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CellTableViewCell
+        cell.label.text = names[indexPath.row]
+        cell.img.image = UIImage(named: names[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
     }
 }
+
+//
+//extension UIViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return names.count
+//    }
+//
+//
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CellTableViewCell
+//        cell?.label.text = names[indexPath.row]
+//        cell?.img.image = UIImage(named: names[indexPath.row])
+//        return cell!
+//    }
+//}
