43d52d51c01cf4fae012da0840b846aa76c8eb5f
eugeniafeng <eugeniafeng@gmail.com>
2020-07-25 18:40:49 -0400

    Images now work in tableView, created segue from cell to next page

--- HopperHacksProject/FirstWriterViewController.swift
+++ HopperHacksProject/FirstWriterViewController.swift
@@ -36,6 +36,10 @@ class FirstWriterViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "cellmove", sender: tableView.cellForRow(at: indexPath))
+    }
 }
 
 //
