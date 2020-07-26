4183c8167fef409cbce92a41bf7118742cf79739
eugeniafeng <eugeniafeng@gmail.com>
2020-07-25 20:41:04 -0400

    fixed bio on elderly people side

--- HopperHacksProject/Receiver/SecondViewController.swift
+++ HopperHacksProject/Receiver/SecondViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var bio: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        bio.text = "From Paradise, California\nRetired helium balloon salesman\nI wear glasses\nEnjoy playing tennis and watching TV"
     }
 
 
